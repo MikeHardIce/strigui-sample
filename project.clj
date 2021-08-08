c1e06536ebb40d6580a163313c5c77288792cc78
MikeHardIce <mikehardice@gmail.com>
2021-08-08 16:55:49 +0100

    adjusted for alpha9

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A little example how to use strigui"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [strigui "0.0.1-alpha7"]
+                 [strigui "0.0.1-alpha9"]
                  [clojure2d "1.4.2"]]
   :main ^:skip-aot strigui-sample.core
   :target-path "target/%s"
--- src/strigui_sample/core.clj
+++ src/strigui_sample/core.clj
@@ -7,18 +7,18 @@
   "Little example"
   [& args]
   (gui/window! 600 600 "Strigui")
-  (gui/label "welcome" "Welcome to Strigui" {:x 190 :y 20
+  (gui/label! "welcome" "Welcome to Strigui" {:x 190 :y 20
                                              :color [0xffaa11]
                                              :font-size 20 :font-style [:bold]})
-  (gui/button "a" "Hello World!" {:x 50 :y 50 :color [:0xff1111 :green]})
-  (gui/button "b" "How are you?" {:x 50 :y 100 :color [0x223344 :blue]
+  (gui/button! "a" "Hello World!" {:x 50 :y 50 :color [:0xff1111 :green] :can-tab? true})
+  (gui/button! "b" "How are you?" {:x 50 :y 100 :color [0x223344 :blue]
                                   :font-size 20 :font-style [:bold]})
-  (gui/button "c" "Blah" {:x 50 :y 150 :color [:blue :yellow] :min-width 100})
-  (gui/button "d" "Bye" {:x 50 :y 200 :color [:yellow :green] :min-width 100})
-  (gui/button "e" "t" {:x 50 :y 250 :color [:green :red]})
-  (gui/input "inp2" "" {:x 350 :y 100 :color [:white :red] :min-width 100})
-  (gui/input "inp3" "last" {:x 350 :y 150 :color [:white :red] :min-width 100})
-  (gui/create (st/->Stack "stacks" '(5 1 8 2 0 3 0 5 7) {:x 100 :y 400}))
+  (gui/button! "c" "Blah" {:x 50 :y 150 :color [:blue :yellow] :min-width 100 :can-tab? true})
+  (gui/button! "d" "Bye" {:x 50 :y 200 :color [:yellow :green] :min-width 100})
+  (gui/button! "e" "t" {:x 50 :y 250 :color [:green :red]})
+  (gui/input! "inp2" "" {:x 350 :y 100 :color [:white :red] :min-width 100})
+  (gui/input! "inp3" "last" {:x 350 :y 150 :color [:white :red] :min-width 100})
+  (gui/create! (st/->Stack "stacks" '(5 1 8 2 0 3 0 5 7) {:x 100 :y 400}))
   (gui/find-by-name "inp2")
   (gui/remove! "inp1")
   (gui/update! "inp3" :value "Hello")
